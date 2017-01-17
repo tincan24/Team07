ac7dd7f4f31d8d10ae368ca84f4d80e8f43660d6
Anant Mahajan <mahajananant@gmail.com>
2017-01-17 15:12:24 -0800

    Added Basic Unit Tests and Corrected Mismatched Curly Brace Error

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,67 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+// TESTING ToString function of NginxConfigStatement
+
+TEST(NginxConfigTest, SimpleTest) {
+	NginxConfigStatement statement;
+	statement.tokens_.push_back("foo");
+	statement.tokens_.push_back("bar");
+	EXPECT_EQ("foo bar;\n", statement.ToString(0));
+}
+
+TEST(NginxConfigTest, BlockTest) {
+	NginxConfigStatement statement;
+	statement.tokens_.push_back("{");
+	statement.tokens_.push_back("foo");
+	statement.tokens_.push_back("bar");
+	statement.tokens_.push_back("}");
+	EXPECT_EQ("{ foo bar };\n", statement.ToString(0));
+}
+
+TEST(NginxConfigTest, EmptyTest) {
+	NginxConfigStatement statement;
+	EXPECT_EQ(";\n", statement.ToString(0));
+}
+
+// TESTING Fixture for NginxConfigs in string format
+
+class NginxStringConfigTest : public ::testing::Test {
+protected:
+	bool ParseString(const std::string config_string){
+		std::stringstream config_stream(config_string);
+		return parser_.Parse(&config_stream, &out_config_);
+	}
+	NginxConfigParser parser_;
+	NginxConfig out_config_;
+};
+
+
+TEST_F(NginxStringConfigTest, SimpleTest) {
+	EXPECT_TRUE(ParseString("server { port: 8080; }"));
+}
+
+TEST_F(NginxStringConfigTest, SimpleWrongTest) {
+	EXPECT_FALSE(ParseString("{ }"));
+}
+
+TEST_F(NginxStringConfigTest, MissingSemicolon) {
+	EXPECT_FALSE(ParseString("server { port: 8080 }"));
+}
+
+TEST_F(NginxStringConfigTest, UnbalancedCurlyBraces) {
+	EXPECT_FALSE(ParseString("server { port: 8080; "));
+}
+
+TEST_F(NginxStringConfigTest, UnbalancedCurlyBracesOpening) {
+	EXPECT_FALSE(ParseString("server port: 8080 }"));
+}
+
+TEST_F(NginxStringConfigTest, UnbalancedCurlyBracesNested) {
+	EXPECT_FALSE(ParseString("server { Location {false; port: 8080 }"));
+}
+
+TEST_F(NginxStringConfigTest, BlockAdvanced) {
+	EXPECT_TRUE(ParseString("server { port { 8080 ;}}"));
+}
