f9baa4ce752ab4b90c31556f0cac217d735ea276
Derek Nguyen <tincan24@gmail.com>
2017-02-25 22:52:47 -0800

    WIP: Implement proxy handler

---
+++ proxy_handler.cpp
@@ -0,0 +1,133 @@
+/* Source:
+    www.boost.org/doc/libs/1_62_0/doc/html/boost_aAsio/example/cpp03/http/client/sync_client.cpp
+*/
+
+#include <iostream>
+#include <cstring>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <vector>
+#include "proxy_handler.hpp"
+#include "config_parser.h"
+
+using boost::asio::ip::tcp;
+
+namespace http {
+namespace server {
+
+ProxyHandler::ProxyHandler() {}
+
+ProxyHandler::~ProxyHandler() {}
+
+RequestHandler::Status ProxyHandler::Init(const std::string& uri_prefix, const NginxConfig& config) {
+    (void) uri_prefix;
+
+    if (config.statements_.size() == 0)
+        return RequestHandler::not_found;
+
+    location_ = config.statements_[0]->tokens_[1];
+    return RequestHandler::OK;
+}
+
+RequestHandler::Status ProxyHandler::HandleRequest(const Request &request, Response* response) {
+    std::string location = location_;
+
+    for(;;) {
+        Response::ResponseCode response_code = RedirectRequest(location, response);
+        //TODO: change return not found
+        if (response_code == Response::ResponseCode::internal_server_error)
+            return RequestHandler::not_found;
+        else if (response_code == Response::ResponseCode::ok)
+            return RequestHandler::OK;
+    }
+    return RequestHandler::OK;
+}
+
+Response::ResponseCode ProxyHandler::RedirectRequest(std::string& location, Response* response) {
+    boost::asio::io_service io_service;
+    tcp::resolver resolver(io_service);
+    std::cout << location << std::endl;
+    tcp::resolver::query query(location, "http");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
+
+    std::string req = "GET / HTTP/1.1\r\nHost: " + location + "\r\nAccept: */*\r\nConnection: close\r\n\r\n";
+    boost::asio::write(socket, boost::asio::buffer(req, req.size()));
+
+    boost::asio::streambuf resp;
+    boost::asio::read_until(socket, resp, "\r\n");
+    
+    std::istream response_stream(&resp);
+    std::string http_version;
+    response_stream >> http_version;
+    unsigned int status_code;
+    response_stream >> status_code;
+    std::string status_message;
+    std::getline(response_stream, status_message);
+    if (!response_stream || http_version.substr(0, 5) != "HTTP/") {
+        std::cout << "Invalid response\n";
+        return Response::ResponseCode::internal_server_error;
+    }
+
+    // Read the response headers, which are terminated by a blank line.
+    boost::asio::read_until(socket, resp, "\r\n\r\n");
+
+    // Process the response headers.
+    std::string header;
+    while (std::getline(response_stream, header) && header != "\r") {
+        size_t colon_pos = header.find(":");
+        std::string key = header.substr(0, colon_pos);
+        std::string value = header.substr(colon_pos + 2);
+
+        if (status_code == 302 && key == "Location") {
+            location = value;
+            return Response::ResponseCode::moved_temporarily;
+        }
+
+        response->AddHeader(key, value);
+    }
+
+    if (status_code != 200) {
+        std::cout << "Response returned with status code " << status_code << "\n";
+        return Response::ResponseCode::internal_server_error;
+    }
+
+    //std::string response_body = "";
+    
+    // Write whatever content we already have to output.
+    /*if (resp.size() > 0) {
+        std::ostringstream ss;
+        ss << &resp;
+        response_body += ss.str();
+    }*/
+
+    // Read until EOF, writing data to output as we go.
+    boost::system::error_code error;
+    // while (boost::asio::read_until(socket, resp, "\n") > 0) {
+    while (boost::asio::read(socket, resp, error)) {
+        std::ostringstream ss;
+        ss << &resp;
+        // std::string s = ss.str();
+        //std::string s((std::istreambuf_iterator<char>(&resp)), std::istreambuf_iterator<char>());
+        response->SetBody(ss.str());
+        // std::cout << s << std::endl;
+        if (error == boost::asio::error::eof) {
+            std::cout << "reached EOF" << std::endl;
+            break;
+        }
+    }
+    //std::cout << response_body << std::endl;
+    // if (error != boost::asio::error::eof)
+    //     std::cout << "Error: " << error << std::endl;
+
+    response->SetStatus(Response::ok);
+    //response->SetBody(response_body);
+    //std::cout << response_body.size() << std::endl;
+    return Response::ResponseCode::ok;
+}
+
+}
+}
---
+++ proxy_handler.hpp
@@ -0,0 +1,24 @@
+#ifndef PROXY_HANDLER_HPP
+#define PROXY_HANDLER_HPP
+
+#include "request_handler.hpp"
+
+namespace http {
+namespace server {
+class ProxyHandler : public RequestHandler {
+public:
+    ProxyHandler();
+    virtual ~ProxyHandler();
+    Status Init(const std::string& uri_prefix, const NginxConfig& config);
+    Status HandleRequest(const Request &request, Response* response);
+private:
+    Response::ResponseCode RedirectRequest(std::string& location, Response* response);
+
+    std::string location_;
+};
+
+REGISTER_REQUEST_HANDLER(ProxyHandler);
+}
+}
+
+#endif
